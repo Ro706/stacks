0a8b9c73920d95b05e10abd1e0c7f374c69e00bb
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2023-11-28 07:54:27 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,82 +1,74 @@
 #include <stdio.h>
-# define SIZE 100
-void enqueue();
-void dequeue();
-void show();
-int inp_arr[SIZE];
-int Rear = - 1;
-int Front = - 1;
-main()
-{
-    int ch;
-    while (1)
-    {
-        printf("1.Enqueue Operation\n");
-        printf("2.Dequeue Operation\n");
-        printf("3.Display the Queue\n");
-        printf("4.Exit\n");
-        printf("Enter your choice of operations : ");
-        scanf("%d", &ch);
-        switch (ch)
-        {
-            case 1:
-            enqueue();
-            break;
-            case 2:
-            dequeue();
-            break;
-            case 3:
-            show();
-            break;
-            case 4:
-            exit(0);
-            default:
-            printf("Incorrect choice \n");
-        } 
-    } 
-} 
- 
-void enqueue()
-{
-    int insert_item;
-    if (Rear == SIZE - 1)
-       printf("Overflow \n");
-    else
-    {
-        if (Front == - 1)
-      
-        Front = 0;
-        printf("Element to be inserted in the Queue\n : ");
-        scanf("%d", &insert_item);
-        Rear = Rear + 1;
-        inp_arr[Rear] = insert_item;
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+int items[MAX_SIZE];
+int front = -1, rear = -1;
+
+int isEmpty() {
+    return (front == -1 && rear == -1);
+}
+
+int isFull() {
+    return (rear == MAX_SIZE - 1);
+}
+
+void enqueue(int value) {
+    if (isFull()) {
+        printf("Queue is full!\n");
+    } else {
+        if (isEmpty()) {
+            front = rear = 0;
+        } else {
+            rear++;
+        }
+        items[rear] = value;
+        printf("Enqueued: %d\n", value);
     }
-} 
- 
-void dequeue()
-{
-    if (Front == - 1 || Front > Rear)
-    {
-        printf("Underflow \n");
-        return ;
+}
+
+int dequeue() {
+    int item;
+    if (isEmpty()) {
+        printf("Queue is empty!\n");
+        return -1;
+    } else {
+        item = items[front];
+        if (front == rear) {
+            front = rear = -1;
+        } else {
+            front++;
+        }
+        printf("Dequeued: %d\n", item);
+        return item;
     }
-    else
-    {
-        printf("Element deleted from the Queue: %d\n", inp_arr[Front]);
-        Front = Front + 1;
-    }
-} 
- 
-void show()
-{
-    
-    if (Front == - 1)
-        printf("Empty Queue \n");
-    else
-    {
-        printf("Queue: \n");
-        for (int i = Front; i <= Rear; i++)
-            printf("%d ", inp_arr[i]);
+}
+
+void display() {
+    if (isEmpty()) {
+        printf("Queue is empty!\n");
+    } else {
+        printf("Queue elements: ");
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", items[i]);
+        }
         printf("\n");
     }
+}
+
+int main() {
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    enqueue(4);
+
+    display();
+
+    dequeue();
+    dequeue();
+
+    display();
+
+    return 0;
 }
